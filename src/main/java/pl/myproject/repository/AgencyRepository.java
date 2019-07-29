a4ab4dc93cddcb870376d854e5181adb726a4cf0
Robert Kachniarz <kachniarz.robert@gmail.com>
2019-07-29 16:41:35 +0200

    Add repository of agencies

--- src/main/java/pl/myproject/repository/AgencyRepository.java
+++ src/main/java/pl/myproject/repository/AgencyRepository.java
@@ -1,4 +1,25 @@
 package pl.myproject.repository;
 
+import pl.myproject.model.RealEstateAgency;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AgencyRepository {
+    private List<RealEstateAgency> agenciesRepository = new ArrayList();
+
+    public AgencyRepository() {
+    }
+
+    public List<RealEstateAgency> getAgenciesRepository() {
+        return agenciesRepository;
+    }
+
+    public void setAgenciesRepository(List<RealEstateAgency> agenciesRepository) {
+        this.agenciesRepository = agenciesRepository;
+    }
+
+    public void addAgency(RealEstateAgency realEstateAgency){
+        this.agenciesRepository.add(realEstateAgency);
+    }
 }
--- src/test/java/pl/myproject/AgencyRepositoryTest.java
+++ src/test/java/pl/myproject/AgencyRepositoryTest.java
@@ -1,4 +1,23 @@
 package pl.myproject;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import pl.myproject.model.RealEstateAgency;
+import pl.myproject.repository.AgencyRepository;
+
 public class AgencyRepositoryTest {
+
+    @Test
+    public void shouldAddAgencyToRepository(){
+        //given
+        RealEstateAgency realEstateAgency = new RealEstateAgency("Agencja 1", "ul. Spokojna 5", "+48601500700");
+        AgencyRepository agencyRepository = new AgencyRepository();
+
+        //when
+        agencyRepository.addAgency(realEstateAgency);
+
+        //then
+        Assertions.assertThat(agencyRepository.getAgenciesRepository()).contains(realEstateAgency);
+    }
+
 }
